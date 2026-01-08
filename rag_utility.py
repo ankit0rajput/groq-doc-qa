ff0a2ef72c8baee1d630b7ffa75cc3d1f840fe02
ankit0rajput <36675436+ankit0rajput@users.noreply.github.com>
2026-01-08 13:26:45 +0530

    Update rag_utility.py

--- rag_utility.py
+++ rag_utility.py
@@ -6,6 +6,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
 # from langchain.chains import RetrievalQA
 
 from langchain_community.vectorstores import Chroma
@@ -22,8 +23,13 @@ working_dir = os.path.dirname(os.path.abspath((__file__)))
 embedding = HuggingFaceEmbeddings()
 
 # Load the Llama-3.3-70B model from Groq
-llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
+# llm = ChatGroq(
+#     model="llama-3.3-70b-versatile",
+#     temperature=0
+# )
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
     temperature=0
 )
 
@@ -45,27 +51,6 @@ def process_document_to_chroma_db(file_name):
         persist_directory=f"{working_dir}/doc_vectorstore"
     )
     return 0
-
-
-# def answer_question(user_question):
-    # Load the persistent Chroma vector database
-    vectordb = Chroma(
-        persist_directory=f"{working_dir}/doc_vectorstore",
-        embedding_function=embedding
-    )
-    # Create a retriever for document search
-    retriever = vectordb.as_retriever()
-
-    # Create a RetrievalQA chain to answer user questions using Llama-3.3-70B
-    qa_chain = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=retriever,
-    )
-    response = qa_chain.invoke({"query": user_question})
-    answer = response["result"]
-
-    return answer
 
 
 def answer_question(user_question):
